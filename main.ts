e325f3bf37600806b567c34051f90e1cceaa9a8d
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2022-01-30 10:21:13 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 //%block="Keyboard" color=#00cbff icon="\uf11c"
 namespace keyboard {
-    export function createKeyboard(result: string, cursorimg?: Image, start_x = 0, start_y = 0, chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", ":", " "]) {
+    export function keyboard(onenter: (input: string) => void, onchange: (input: string) => void, cursorimg?: Image, chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", ":", "/", "+", "-", "*", ">", "\\", "[", "]", "{", "}", "|", ";", "(", ")", "^", "%", "$", "!", "#", "^", "<", " "]) {
         pause(200)
         let input = ""
         let cursor = sprites.create(cursorimg)
-        let cursor_x = 10 + start_x
-        let cursor_y = 80 + start_y
+        let cursor_x = 10
+        let cursor_y = 80
         cursor.setPosition(cursor_x, cursor_y)
         let cursor_pos_x = [0]
         let cursor_pos_y = [0]
         let cursor_chars = [{ x: 0, y: 0, char: "" }]
         let btns = [textsprite.create("")]
-        let x = start_x
-        let y = start_y
+        let x = 0
+        let y = 0
         let x_reset = false
         let old_y = y
         cursor_pos_x.splice(0, 1)
@@ -22,14 +22,14 @@ namespace keyboard {
         btns.splice(0, 1)
         for (let char = 0; char < chars.length; char++) {
             let btn = textsprite.create(chars[char])
-            y = (Math.floor(char / 21) * 8) + start_y
+            y = Math.floor(char / 21) * 8
             if (old_y != y) {
                 old_y = y
                 x_reset = true
             }
             x += 1
             if (x_reset) {
-                x = start_x + 1
+                x = 1
                 x_reset = false
             }
             btn.setPosition(3 + (x * 7), 80 + y)
@@ -45,31 +45,31 @@ namespace keyboard {
             btns.push(btn)
         }
         let enter_btn = sprites.create(img`
-            . . . . . . .
-            . . . . 1 . .
-            . . . . . 1 .
-            . 1 1 1 1 1 1
-            . . . . . 1 .
-            . . . . 1 . .
-            . . . . . . .
-        `)
-        enter_btn.setPosition(150 + start_x, 88 + start_y)
-        cursor_pos_x.push(150 + start_x)
-        cursor_pos_y.push(88 + start_y)
-        cursor_chars.push({ x: 150 + start_x, y: 88 + start_y, char: "enter" })
+        . . . . . . .
+        . . . . 1 . .
+        . . . . . 1 .
+        . 1 1 1 1 1 1
+        . . . . . 1 .
+        . . . . 1 . .
+        . . . . . . .
+    `)
+        enter_btn.setPosition(150, 96)
+        cursor_pos_x.push(150)
+        cursor_pos_y.push(96)
+        cursor_chars.push({ x: 150, y: 96, char: "enter" })
         let back_btn = sprites.create(img`
-            . . . . . . .
-            . . 1 . . . .
-            . 1 . . . . .
-            1 1 1 1 1 1 .
-            . 1 . . . . .
-            . . 1 . . . .
-            . . . . . . .
-        `)
-        back_btn.setPosition(143, 88)
+        . . . . . . .
+        . . 1 . . . .
+        . 1 . . . . .
+        1 1 1 1 1 1 .
+        . 1 . . . . .
+        . . 1 . . . .
+        . . . . . . .
+    `)
+        back_btn.setPosition(143, 96)
         cursor_pos_x.push(143)
-        cursor_pos_y.push(88)
-        cursor_chars.push({ x: 143 + start_x, y: 88 + start_y, char: "back" })
+        cursor_pos_y.push(96)
+        cursor_chars.push({ x: 143, y: 96, char: "back" })
         forever(function () {
             if (controller.right.isPressed() && cursor_pos_x.indexOf(cursor_x + 7) != -1) {
                 cursor_x += 7
@@ -92,6 +92,7 @@ namespace keyboard {
                     if (cursor_x == cursor_chars[char].x && cursor_y == cursor_chars[char].y) {
                         if (cursor_chars[char].char != "enter" && cursor_chars[char].char != "back") {
                             input += cursor_chars[char].char
+                            onchange(input)
                             pause(200)
                         } else {
                             if (cursor_chars[char].char != "back") {
@@ -99,12 +100,11 @@ namespace keyboard {
                                 for (let btn = 0; btn < btns.length; btn++) {
                                     btns[btn].destroy()
                                 }
-                                enter_btn.destroy()
-                                back_btn.destroy()
-                                result = input
-                                break
+                                onenter(input)
+                                input = ""
                             } else {
                                 input = input.slice(0, -1)
+                                onchange(input)
                                 pause(200)
                             }
                         }
