f1bdebb7813119d28032de6fc20d341e7a5b4345
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2022-01-30 10:21:34 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-//%block="Keyboard" color=#00cbff icon="\uf11c"
 namespace keyboard {
     export function keyboard(onenter: (input: string) => void, onchange: (input: string) => void, cursorimg?: Image, chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", ":", "/", "+", "-", "*", ">", "\\", "[", "]", "{", "}", "|", ";", "(", ")", "^", "%", "$", "!", "#", "^", "<", " "]) {
         pause(200)
